c0df55b996f7206fbecbd97848a58781b7b4cdaf
Milind Mishra <milind.mishra4@gmail.com>
2021-10-25 10:17:15 +0530

    divisiblity

--- divisiblebyxfrommton.c
+++ divisiblebyxfrommton.c
@@ -5,9 +5,8 @@ int main() {
     printf("Enter the value of m, n and x: ");
     scanf("%d %d %d", &m, &n, &x);
     for (int i = m; i <= n; i++) {
-        if (i % x == 0) {
-            count++;
-        }
+        if (i % x == 0) // if i is divisible by x
+            count++; // increase count
     }
     printf("The numbers divisible by %d from %d to %d is %d", x, m, n, count);
     return 0;
