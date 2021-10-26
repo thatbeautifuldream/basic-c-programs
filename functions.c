d2ce3435a769797269629a421a645fa4ce09ed0c
Milind Mishra <milind.mishra4@gmail.com>
2021-10-26 17:32:11 +0530

    functions

---
+++ functions.c
@@ -0,0 +1,19 @@
+/* functions in c programming
+(1). function declaration : function name and parameters
+
+<return type> function_name/id (argument_datatype argument_id , ...)
+{
+    // body of the function
+}
+
+two types of function declarations : global and local
+
+(2). function call : function name and arguments
+
+function_name/id (argument_val1, argument_val2, ...);
+
+*/
+#include <stdio.h>
+int main(){
+
+}
